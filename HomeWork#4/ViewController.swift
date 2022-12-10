9d8a1369101066c848a526165c7cd719f0e22491
Semen Vinnikov <semvinnikov@yandex.ru>
2022-12-10 23:13:53 +0300

    Initial Commit

---
+++ HomeWork#4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork#4
+//
+//  Created by Семён Винников on 10.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ HomeWork#4UITests/HomeWork_4UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  HomeWork_4UITestsLaunchTests.swift
+//  HomeWork#4UITests
+//
+//  Created by Семён Винников on 10.12.2022.
+//
+
+import XCTest
+
+final class HomeWork_4UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
